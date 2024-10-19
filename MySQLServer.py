fc903f3e0787fb64e2e70cc51b1cd47346e04633
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-10-19 19:40:19 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -8,7 +8,7 @@ conobj = mysql.connector.connect(
 
 cursor = conobj.cursor()
 try: 
-    cursor.execute("CREATE DATABASE alx_book_store")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 except mysql.connector.Error as err:
     print(f"Failed creating database: {err}")
 print("Database 'alx_book_store' created successfully!")
