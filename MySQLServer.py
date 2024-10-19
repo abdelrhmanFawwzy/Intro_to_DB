c6f8cd6b0d402769f45a5735b060d421b786f412
abdelrhmanFawwzy <110397608+abdelrhmanFawwzy@users.noreply.github.com>
2024-10-19 19:39:16 +0300

    Add files via upload

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+conobj = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root"
+)
+
+cursor = conobj.cursor()
+try: 
+    cursor.execute("CREATE DATABASE alx_book_store")
+except mysql.connector.Error as err:
+    print(f"Failed creating database: {err}")
+print("Database 'alx_book_store' created successfully!")
